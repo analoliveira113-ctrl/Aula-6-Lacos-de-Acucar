399c7ddde7d770ac8a2edd2c42e14e7c284d21ac
Ana Laura Paulino Oliveira <ana.l.oliveira113@aluno.senai.br>
2026-05-05 08:55:51 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ const ASSETS = [
     './assets/images/lacos-trufados.png',
     './assets/images/logo.png',
     './assets/images/whatsapp.png',
-    './assets/images/Sem nome(64 x 64px).png',
+    
     
 
 ];
